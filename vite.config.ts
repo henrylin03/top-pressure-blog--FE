a33c3ae0b3767cfc24b7bca10f2c9a8b01704cca
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-13 16:52:02 +1100

    test: add test config to vite.config.ts file. add setup file.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -10,4 +9,9 @@ export default defineConfig({
       },
     }),
   ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.mjs",
+  },
 });
