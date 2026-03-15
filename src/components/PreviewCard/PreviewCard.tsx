2017fa3c29a11e8472ef93e23acdaeb1100d0755
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-15 13:11:59 +1100

    feat(PreviewCard): format and style posted date for blog preview
    used dayjs for this - lightweight and abstracts away needing to manipulate the date object myself

--- src/components/PreviewCard/PreviewCard.tsx
+++ src/components/PreviewCard/PreviewCard.tsx
@@ -1,5 +1,6 @@
 import { Anchor, Card, Stack } from "@mantine/core";
 import { Link } from "@tanstack/react-router";
+import dayjs from "dayjs";
 import type { Post } from "@/data/fakePosts";
 import styles from "./PreviewCard.module.css";
 
@@ -7,23 +8,29 @@ interface Props {
 	post: Post;
 }
 
-const PreviewCard = ({ post }: Props) => (
-	<Card
-		shadow="xs"
-		withBorder
-		component={Link}
-		to={post.linkToFullText}
-		className={styles.card}
-	>
-		<Stack component="header" gap="xs">
-			<h3 className={styles.heading}>{post.title}</h3>
-			<p>{String(post.postedDate)}</p>
-		</Stack>
-		<p>{post.lede}</p>
-		<Anchor href={post.linkToFullText} underline="hover">
-			Read more
-		</Anchor>
-	</Card>
-);
+const PreviewCard = ({ post }: Props) => {
+	const postedDateFormatted: string = dayjs(post.postedDate).format(
+		"D MMMM YYYY",
+	);
+
+	return (
+		<Card
+			shadow="xs"
+			withBorder
+			component={Link}
+			to={post.linkToFullText}
+			className={styles.card}
+		>
+			<Stack component="header" gap="xs">
+				<h3 className={styles.heading}>{post.title}</h3>
+				<p className={styles.postedDate}>{postedDateFormatted}</p>
+			</Stack>
+			<p>{post.lede}</p>
+			<Anchor href={post.linkToFullText} underline="hover">
+				Read more
+			</Anchor>
+		</Card>
+	);
+};
 
 export default PreviewCard;
