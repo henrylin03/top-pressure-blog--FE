7168a8846d03fa1882e49e61f29113516e7b82ed
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-15 11:29:36 +1100

    feat(theme): adjust default 'black' colour to be in theme with logo, as well as primary colour

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@mantine/core";
+
+export const COLOURS = {
+	primary: "#3895a0",
+	secondary: "#111f30",
+	black: "#09101a",
+};
+
+export const theme = createTheme({
+	primaryColor: "cyan",
+	black: COLOURS.black,
+});
