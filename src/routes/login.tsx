1c06d0e0d85680f60b7391efc8eaae6faed951ba
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-16 12:17:50 +1100

    feat(Login): add email/user and password inputs to card on login page

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,11 +1,18 @@
-import { Card, Container, Stack } from "@mantine/core";
+import {
+	Button,
+	Card,
+	Container,
+	PasswordInput,
+	Stack,
+	TextInput,
+} from "@mantine/core";
 import { createFileRoute, Link } from "@tanstack/react-router";
 import styles from "@/styles/Login.module.css";
 import logoImg from "/images/logo.png";
 
 const LoginPage = () => (
 	<Container>
-		<Card shadow="sm" maw="40rem" mx="auto">
+		<Card shadow="sm" maw="28rem" mx="auto" py="xl" px="5rem">
 			<Stack gap="xl" align="center">
 				<Stack component="header" align="center" gap={0}>
 					<Link to="/" aria-label="Return to home page">
@@ -19,6 +26,26 @@ const LoginPage = () => (
 					</Link>
 					<h2 className={styles.heading}>Welcome back</h2>
 				</Stack>
+
+				<form className={styles.form} method="POST" action="/api/login">
+					<Stack component="ul" className={styles.inputs}>
+						<li>
+							<TextInput
+								placeholder="Email or username"
+								required
+								name="usernameOrEmail"
+							/>
+						</li>
+						<li>
+							<PasswordInput placeholder="Password" required name="password" />
+						</li>
+						<li>
+							<Button size="md" fullWidth type="submit">
+								Sign in
+							</Button>
+						</li>
+					</Stack>
+				</form>
 			</Stack>
 		</Card>
 	</Container>
