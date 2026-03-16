fe091a1446b5a63efd30e04c7f141510f063ddfc
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-16 14:06:28 +1100

    feat(login): submit handler uses fetch to get a response
    if correct, gets 200 status. uses FormData object as well to get the values from the fields. opted to not overengineer and use mantine's form library nor even useState for the values to be controlled

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -11,58 +11,82 @@ import { createFileRoute, Link } from "@tanstack/react-router";
 import styles from "@/styles/Login.module.css";
 import logoImg from "/images/logo.png";
 
-const LoginPage = () => (
-	<Container mt="5rem">
-		<Card
-			shadow="sm"
-			maw="24rem"
-			m="auto"
-			pt="xl"
-			pb="5rem"
-			px="2.5rem"
-			radius="lg"
-		>
-			<Stack align="center" gap="lg">
-				<Stack component="header" align="center" gap={0}>
-					<Link to="/" aria-label="Return to home page">
-						<img
-							src={logoImg}
-							alt="Logo of Top Pressure Blog"
-							width="80"
-							height="80"
-							loading="eager"
-						/>
-					</Link>
-					<h2 className={styles.heading}>Welcome back</h2>
-				</Stack>
+const LoginPage = () => {
+	const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
+		e.preventDefault();
 
-				<form className={styles.form} method="POST" action="/api/login">
-					<Stack component="ul" className={styles.inputs}>
-						<li>
-							<TextInput
-								placeholder="Email or username"
-								required
-								name="usernameOrEmail"
+		const formDataObject = new FormData(e.currentTarget);
+		const response = await fetch("/api/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				usernameOrEmail: formDataObject.get("usernameOrEmail"),
+				password: formDataObject.get("password"),
+			}),
+		});
+
+		console.log("response:", response);
+	};
+
+	return (
+		<Container mt="5rem">
+			<Card
+				shadow="sm"
+				maw="24rem"
+				m="auto"
+				pt="xl"
+				pb="5rem"
+				px="2.5rem"
+				radius="lg"
+			>
+				<Stack align="center" gap="lg">
+					<Stack component="header" align="center" gap={0}>
+						<Link to="/" aria-label="Return to home page">
+							<img
+								src={logoImg}
+								alt="Logo of Top Pressure Blog"
+								width="80"
+								height="80"
+								loading="eager"
 							/>
-						</li>
-						<li>
-							<PasswordInput placeholder="Password" required name="password" />
-						</li>
-						<li>
-							<Button size="md" fullWidth type="submit">
-								Sign in
-							</Button>
-						</li>
+						</Link>
+						<h2 className={styles.heading}>Welcome back</h2>
 					</Stack>
-				</form>
 
-				<Text fz="xs" c="dimmed">
-					Don't have an account? <Link to="/signup">Sign up</Link>
-				</Text>
-			</Stack>
-		</Card>
-	</Container>
-);
+					<form className={styles.form} onSubmit={handleSubmit}>
+						<Stack component="ul" className={styles.inputs}>
+							<li>
+								<TextInput
+									placeholder="Email or username"
+									required
+									name="usernameOrEmail"
+								/>
+							</li>
+							<li>
+								<PasswordInput
+									placeholder="Password"
+									required
+									name="password"
+								/>
+							</li>
+							<li>
+								<Button size="md" fullWidth type="submit">
+									Sign in
+								</Button>
+							</li>
+						</Stack>
+					</form>
+
+					<Text fz="xs" c="dimmed">
+						Don't have an account? <Link to="/signup">Sign up</Link>
+					</Text>
+				</Stack>
+			</Card>
+		</Container>
+	);
+};
 
 export const Route = createFileRoute("/login")({
 	component: LoginPage,
