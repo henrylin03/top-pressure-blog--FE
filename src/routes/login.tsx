3eb319d06fde858b6e85be3ccbef3c66a55f8976
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-16 12:06:10 +1100

    feat(Login): add login card, with title and styling

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,8 +1,28 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { Card, Container, Stack } from "@mantine/core";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import styles from "@/styles/Login.module.css";
+import logoImg from "/images/logo.png";
 
-const LoginPage = () => {
-	return <div>Hello "/login"!</div>;
-};
+const LoginPage = () => (
+	<Container>
+		<Card shadow="sm" maw="40rem" mx="auto">
+			<Stack gap="xl" align="center">
+				<Stack component="header" align="center" gap={0}>
+					<Link to="/" aria-label="Return to home page">
+						<img
+							src={logoImg}
+							alt="Logo of Top Pressure Blog"
+							width="80"
+							height="80"
+							loading="eager"
+						/>
+					</Link>
+					<h2 className={styles.heading}>Welcome back</h2>
+				</Stack>
+			</Stack>
+		</Card>
+	</Container>
+);
 
 export const Route = createFileRoute("/login")({
 	component: LoginPage,
