93f623cf7fa1e2a74285113694d311490f30de44
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-17 20:22:17 +1100

    feat(ProfileMenu): on sign out, user is navigated back to home page
    at the moment, these navigates (useNavigate hooks) are called in click handlers, rather than at the hook level. when i implement behaviour that user is to be taken back to their previous page, then i can do it perhaps at the hook level

--- src/components/Header/ProfileMenu/ProfileMenu.tsx
+++ src/components/Header/ProfileMenu/ProfileMenu.tsx
@@ -1,26 +1,36 @@
 import { Group, Menu, Text } from "@mantine/core";
+import { useNavigate } from "@tanstack/react-router";
 import ProfileButton from "./ProfileButton";
 import UserAvatar from "./UserAvatar";
 
 type Props = Readonly<{ username: string; email: string; logout: () => void }>;
 
-const ProfileMenu = ({ username, email, logout }: Props) => (
-	<Menu withArrow width={180}>
-		<Menu.Target>
-			<ProfileButton username={username} email={email} />
-		</Menu.Target>
-		<Menu.Dropdown>
-			<Menu.Label>
-				<Group>
-					<UserAvatar username={username} size="md" />
-					<Text c="dark.7">{username}</Text>
-				</Group>
-			</Menu.Label>
+const ProfileMenu = ({ username, email, logout }: Props) => {
+	const navigate = useNavigate();
 
-			<Menu.Divider />
-			<Menu.Item onClick={logout}>Log out</Menu.Item>
-		</Menu.Dropdown>
-	</Menu>
-);
+	const handleClickOnSignOut = () => {
+		logout();
+		navigate({ to: "/" });
+	};
+
+	return (
+		<Menu withArrow width={180}>
+			<Menu.Target>
+				<ProfileButton username={username} email={email} />
+			</Menu.Target>
+			<Menu.Dropdown>
+				<Menu.Label>
+					<Group>
+						<UserAvatar username={username} size="md" />
+						<Text c="dark.7">{username}</Text>
+					</Group>
+				</Menu.Label>
+
+				<Menu.Divider />
+				<Menu.Item onClick={handleClickOnSignOut}>Sign out</Menu.Item>
+			</Menu.Dropdown>
+		</Menu>
+	);
+};
 
 export default ProfileMenu;
