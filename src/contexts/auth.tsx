e0280644fe909b622eef3692d06ddfc592c14623
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-18 09:31:39 +1100

    fix(auth): prevent re-validating after every render
    i had incorrectly removed the dependency array from useEffect. keeping it empty would've been correct, except for the fact that the linter is telling me i need to put dependency inside. putting in the validateJwt method into dependency array also threw error, so have to wrap it in useCallback with empty dependency array to keep it stable, caching it between re-renders iiuc

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,4 +1,10 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import {
+	createContext,
+	useCallback,
+	useContext,
+	useEffect,
+	useState,
+} from "react";
 import type { User } from "@/types/user";
 
 export type AuthState = {
@@ -8,6 +14,8 @@ export type AuthState = {
 	logout: () => void;
 };
 
+const JWT_LOCALSTORAGE_KEY = "jwt";
+
 const AuthContext = createContext<AuthState | undefined>(undefined);
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
@@ -15,25 +23,23 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 	const [isAuthenticated, setIsAuthenticated] = useState(false);
 	const [isLoading, setIsLoading] = useState(true);
 
-	const JWT_LOCALSTORAGE_KEY = "jwt";
-
-	const validateJwt = async (localStorageKey: string, token: string) => {
+	const validateJwt = useCallback(async (token: string) => {
 		const res = await fetch("/api/validate-jwt", {
 			headers: { Authorization: `Bearer ${token}` },
 		});
-		if (!res.ok) return localStorage.removeItem(localStorageKey);
+		if (!res.ok) return localStorage.removeItem(JWT_LOCALSTORAGE_KEY);
 
 		const { user } = await res.json();
-		if (!user) return localStorage.removeItem(localStorageKey); // not authenticated
+		if (!user) return localStorage.removeItem(JWT_LOCALSTORAGE_KEY);
 		setUser(user);
 		setIsAuthenticated(true);
-	};
+	}, []);
 
 	useEffect(() => {
 		const token = localStorage.getItem(JWT_LOCALSTORAGE_KEY);
 		if (!token) return setIsLoading(false);
-		validateJwt(JWT_LOCALSTORAGE_KEY, token).finally(() => setIsLoading(false));
-	});
+		validateJwt(token).finally(() => setIsLoading(false));
+	}, [validateJwt]);
 
 	if (isLoading)
 		return (
@@ -53,7 +59,7 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 		const { token } = await loginRes.json();
 		localStorage.setItem(JWT_LOCALSTORAGE_KEY, token);
 
-		await validateJwt(JWT_LOCALSTORAGE_KEY, token);
+		await validateJwt(token);
 	};
 
 	const logout = () => {
