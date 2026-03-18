41a5a50825b08d11c80dee538e7e2434449f3203
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-18 14:13:57 +1100

    feat(post): get postId from route params

--- src/routes/posts/$postId.tsx
+++ src/routes/posts/$postId.tsx
@@ -1,9 +1,31 @@
+import { Container, Title } from "@mantine/core";
 import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+import styles from "@/styles/Post.module.css";
+import type { PublishedPost } from "@/types/post";
 
 export const Route = createFileRoute("/posts/$postId")({
-	component: RouteComponent,
+	component: PostPage,
 });
 
-function RouteComponent() {
-	return <div>Hello "/post/$postId"!</div>;
+function PostPage() {
+	const [_post, _setPost] = useState<PublishedPost | null>(null);
+
+	const { postId } = Route.useParams();
+	console.log(postId);
+
+	// useEffect(() => {
+
+	// }, []);
+
+	return (
+		<Container>
+			<Container component="section" className={styles.section}>
+				<Title order={2} size="h1" fw={400}>
+					The Deep Half
+				</Title>
+			</Container>
+			<Container component="section" className={styles.section}></Container>
+		</Container>
+	);
 }
