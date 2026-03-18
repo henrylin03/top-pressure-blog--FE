0bef049b29c541e10dc641d47fa3a01b17012c9d
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-18 14:37:49 +1100

    feat(post): fetch post from server using useEffect
    also lightly handle errors by throwing error if post doesn't exist, but #20 will handle this a bit better. currently, just print the entire object into the jsx

--- src/routes/posts/$postId.tsx
+++ src/routes/posts/$postId.tsx
@@ -1,6 +1,7 @@
-import { Container, Title } from "@mantine/core";
+import { Container } from "@mantine/core";
 import { createFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { JWT_LOCALSTORAGE_KEY } from "@/contexts/auth";
 import styles from "@/styles/Post.module.css";
 import type { PublishedPost } from "@/types/post";
 
@@ -9,21 +10,34 @@ export const Route = createFileRoute("/posts/$postId")({
 });
 
 function PostPage() {
-	const [_post, _setPost] = useState<PublishedPost | null>(null);
-
+	const [post, setPost] = useState<PublishedPost | null>(null);
+	const [isLoading, setIsLoading] = useState(true);
 	const { postId } = Route.useParams();
-	console.log(postId);
 
-	// useEffect(() => {
+	useEffect(() => {
+		const token = localStorage.getItem(JWT_LOCALSTORAGE_KEY);
 
-	// }, []);
+		fetch(`/api/posts/${postId}`, {
+			headers: { Authorization: token ? `Bearer ${token}` : "" },
+		})
+			.then((res) => res.json())
+			.then((json) => {
+				if (!json.post) throw new Error("404: Post not found");
+				setPost(json.post);
+			})
+			.catch((err) => console.error(err))
+			.finally(() => setIsLoading(false));
+	}, [postId]);
+
+	if (isLoading) return <Container>Loading...</Container>;
 
 	return (
 		<Container>
 			<Container component="section" className={styles.section}>
-				<Title order={2} size="h1" fw={400}>
+				{/* <Title order={2} size="h1" fw={400}>
 					The Deep Half
-				</Title>
+				</Title> */}
+				<p>{JSON.stringify(post)}</p>
 			</Container>
 			<Container component="section" className={styles.section}></Container>
 		</Container>
