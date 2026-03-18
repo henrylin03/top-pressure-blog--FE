7ad20993d1fa596a8224ef2ea545825a94f5ec4a
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-18 15:26:51 +1100

    feat(PostHeader): style, including formatting published date with dayjs

--- src/components/post/Header.tsx
+++ src/components/post/Header.tsx
@@ -1,5 +1,6 @@
 import { Anchor, Group, Text, Title } from "@mantine/core";
 import { IconHourglassHigh } from "@tabler/icons-react";
+import dayjs from "dayjs";
 import type { PublishedPost } from "@/types/post";
 
 type PostHeaderDetails = Pick<
@@ -11,22 +12,34 @@ interface Props {
 	postDetails: PostHeaderDetails;
 }
 
-const PostHeader = ({ postDetails }: Props) => (
-	<>
-		<Group component={Text} fz="sm" c="dimmed" gap={8}>
-			<IconHourglassHigh size={16} />
-			<span>{Math.ceil(postDetails.timeToReadInMinutes)} min read</span>
-		</Group>
-		<Title order={2} size="h1" fw={400}>
-			{postDetails.title}
-		</Title>
-		<Text>
-			{String(postDetails.publishedAt)} by{" "}
-			<Anchor href="https://henrylin.io" target="_blank">
-				Henry Lin
-			</Anchor>
-		</Text>
-	</>
-);
+const PostHeader = ({ postDetails }: Props) => {
+	const timeToReadRounded = Math.ceil(postDetails.timeToReadInMinutes);
+	const publishedDateFormatted = dayjs(String(postDetails.publishedAt)).format(
+		"d MMMM YYYY",
+	);
+
+	return (
+		<>
+			<Group component={Text} fz="sm" c="dark.3" gap={8}>
+				<IconHourglassHigh size={16} />
+				<span>{timeToReadRounded} min read</span>
+			</Group>
+			<Title order={2} size="h1" fw={400}>
+				{postDetails.title}
+			</Title>
+			<Text mt="md" c="dark.4">
+				{publishedDateFormatted} by{" "}
+				<Anchor
+					href="https://henrylin.io"
+					target="_blank"
+					underline="not-hover"
+					fw="bold"
+				>
+					Henry Lin
+				</Anchor>
+			</Text>
+		</>
+	);
+};
 
 export default PostHeader;
