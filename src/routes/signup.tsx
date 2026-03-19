4e5d7e46e33d9be5cd5167f168d147ef762503ca
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-19 16:37:17 +1100

    feat(signup): rename component for clarity

--- src/routes/signup.tsx
+++ src/routes/signup.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/signup")({
-	component: RouteComponent,
+	component: SignUpForm,
 });
 
-function RouteComponent() {
+function SignUpForm() {
 	return <div>Hello "/signup"!</div>;
 }
