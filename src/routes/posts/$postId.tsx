ddcb2f57ed35da509fe941f43ac4d434a9712150
Henry <83106787+henrylin03@users.noreply.github.com>
2026-03-19 19:47:26 +1100

    feat(404): build 404 page (#33)
    * feat(ErrorComponent): build 404 page
    
    make somewhat responsive too. this gets shown in any not found error pages from the root (eg /asdf). for /posts/asdf, this will only show the main content section, and not header nor footer.
    
    * refactor: fetchPost is standalone function, that gets called in the loader of the route. this allows all post pages that can't get a post to also show the 404 page with header and footer.
    
    Squashed commit of the following:
    
    commit fe78017a7d7b2d498956f47d23b94905d7bef018
    Author: Henry Lin <83106787+henrylin03@users.noreply.github.com>
    Date:   Thu Mar 19 19:41:26 2026 +1100
    
        refactor(comments): adding new comment calls router.invalidate()
    
        this saves the prop-drilling of the fetchPost, which has changed shape now that it has come out of the component. calling .invalidate refreshes anyway, and can confirm that, after adding a new comment, it displays in list of comments
    
    commit b7deb45b19a63295f5b9847c8346e3fdf653acb4
    Author: Henry Lin <83106787+henrylin03@users.noreply.github.com>
    Date:   Thu Mar 19 19:36:29 2026 +1100
    
        refactor(PostPage): fetchPost is a standalone function
    
        it is still asynchronous, but returns Promise<Post>. this allows it to be used in the loader part of the Route. throw notFound error through if Post not found, or if res.ok is false.

--- src/routes/posts/$postId.tsx
+++ src/routes/posts/$postId.tsx
@@ -1,7 +1,6 @@
 import { Container } from "@mantine/core";
 import { useDocumentTitle } from "@mantine/hooks";
-import { createFileRoute } from "@tanstack/react-router";
-import { useCallback, useEffect, useState } from "react";
+import { createFileRoute, notFound } from "@tanstack/react-router";
 import PostBody from "@/components/post/Body";
 import CommentsSection from "@/components/post/comments/CommentsSection";
 import PostHeader from "@/components/post/Header";
@@ -9,52 +8,29 @@ import { JWT_LOCALSTORAGE_KEY } from "@/contexts/auth";
 import styles from "@/styles/Post.module.css";
 import type { Post } from "@/types/post";
 
-export const Route = createFileRoute("/posts/$postId")({
-	component: PostPage,
-});
-
 const DEFAULT_DOCUMENT_TITLE =
 	"BJJ, Wrestling, Judo | Top Pressure Blog for Submission Grapplers";
 const DOCUMENT_TITLE_SUFFIX = "Top Pressure Blog for Submission Grapplers";
 
+async function fetchPost(postId: string): Promise<Post> {
+	const jwt = localStorage.getItem(JWT_LOCALSTORAGE_KEY) || "";
+	const res = await fetch(`/api/posts/${postId}`, {
+		headers: { Authorization: jwt ? `Bearer ${jwt}` : "" },
+	});
+	if (!res.ok) throw notFound();
+
+	const { post } = await res.json();
+	if (!post) throw notFound();
+
+	return post;
+}
+
 function PostPage() {
-	const [post, setPost] = useState<Post | null>(null);
-	const [isLoading, setIsLoading] = useState(true);
+	const { post } = Route.useLoaderData();
 	const { postId } = Route.useParams();
 	useDocumentTitle(
 		post ? `${post.title} | ${DOCUMENT_TITLE_SUFFIX}` : DEFAULT_DOCUMENT_TITLE,
 	);
-
-	const fetchPost = useCallback(async (postId: string, jwt: string) => {
-		try {
-			const res = await fetch(`/api/posts/${postId}`, {
-				headers: { Authorization: jwt ? `Bearer ${jwt}` : "" },
-			});
-			if (!res.ok) throw new Error("404: post not found.");
-
-			const { post } = await res.json();
-			if (!post) throw new Error("404: post not found");
-			setPost(post);
-		} catch (err) {
-			console.error(err);
-		} finally {
-			setIsLoading(false);
-		}
-	}, []);
-
-	useEffect(() => {
-		const token = localStorage.getItem(JWT_LOCALSTORAGE_KEY) || "";
-		fetchPost(postId, token);
-	}, [postId, fetchPost]);
-
-	if (isLoading) return <Container>Loading...</Container>;
-
-	if (!post)
-		return (
-			<Container>
-				An error has occurred when fetching blog post. Please refresh the page.
-			</Container>
-		);
 
 	const {
 		title,
@@ -82,12 +58,16 @@ function PostPage() {
 				<PostBody lede={lede} text={text} />
 			</Container>
 			<Container component="section" className={styles.section}>
-				<CommentsSection
-					comments={comments}
-					postId={postId}
-					fetchPost={fetchPost}
-				/>
+				<CommentsSection comments={comments} postId={postId} />
 			</Container>
 		</Container>
 	);
 }
+
+export const Route = createFileRoute("/posts/$postId")({
+	component: PostPage,
+	loader: async ({ params: { postId } }) => {
+		const post = await fetchPost(postId);
+		return { post };
+	},
+});
