487ef3ebb3d4361dcf4480ea660520d8982afebb
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-04-21 11:36:01 +1000

    chore: strip all components, routes and uninstall everything tanstack related.
    essentially will start from scratch, and then bring back files that i find are actually required. tanstack router components (like <Link>) was way too embedded into every component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
 import { MantineProvider } from "@mantine/core";
-import { RouterProvider } from "@tanstack/react-router";
-import { AuthProvider, useAuth } from "./contexts/auth";
-import { router } from "./router";
 import { theme } from "./styles/theme";
-import "./styles/global.css";
 import "@mantine/core/styles.css";
 
-const InnerApp = () => {
-	const auth = useAuth();
-	return <RouterProvider router={router} context={{ auth }} />;
-};
-
-const App = () => (
-	<MantineProvider theme={theme}>
-		<AuthProvider>
-			<InnerApp />
-		</AuthProvider>
-	</MantineProvider>
-);
+const App = () => <MantineProvider theme={theme}>hello</MantineProvider>;
 
 export default App;
