9eebe179d4df92538a0f03f3abc29e0c8ac6a533
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-04-22 14:51:53 +1000

    chore: setup basic test (though with errors) for Posts. add MemoryRouter (for React Router) and AuthProvider contexts.
    getting an error relating to localStorage not being a function...

--- test-utils/render.tsx
+++ test-utils/render.tsx
@@ -1,10 +1,16 @@
 import { MantineProvider } from "@mantine/core";
 import { render as testingLibraryRender } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import { AuthProvider } from "@/contexts/auth";
 
 export function render(ui: React.ReactNode) {
 	return testingLibraryRender(ui, {
 		wrapper: ({ children }: { children: React.ReactNode }) => (
-			<MantineProvider env="test">{children}</MantineProvider>
+			<MemoryRouter>
+				<MantineProvider env="test">
+					<AuthProvider>{children}</AuthProvider>
+				</MantineProvider>
+			</MemoryRouter>
 		),
 	});
 }
