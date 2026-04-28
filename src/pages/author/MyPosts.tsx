eb6d77ef97c7867cf9185fedb54c7c0818214cff
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-04-28 15:12:27 +1000

    feat(MyPosts): add header
    includes title of page and the button to write post (main CTA)

--- src/pages/author/MyPosts.tsx
+++ src/pages/author/MyPosts.tsx
@@ -1,3 +1,19 @@
+import { Button, Container, Group, Stack, Title } from "@mantine/core";
+import { IconBallpen } from "@tabler/icons-react";
+
 export default function MyPostsPage() {
-	return <>My posts</>;
+	return (
+		<Container my="xl">
+			<Stack gap="xl">
+				<Group justify="space-between" align="center" component="header">
+					<Title order={2} size="h1">
+						My Posts
+					</Title>
+					<Button leftSection={<IconBallpen size={20} />} size="md" mt="xs">
+						Write post
+					</Button>
+				</Group>
+			</Stack>
+		</Container>
+	);
 }
