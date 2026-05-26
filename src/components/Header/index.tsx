9a9e777f4a824da625af14b3daa6d59a5d0107c6
Henry <83106787+henrylin03@users.noreply.github.com>
2026-05-26 11:01:05 +1000

    feat(Header): if user is an author, then they see 'Editor' in their header (#70)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Group, Paper, Title } from "@mantine/core";
+import { Divider, Group, Paper, Title } from "@mantine/core";
 import { Link } from "react-router";
 import { useAuth } from "@/contexts/auth";
 import logo from "/images/logo.png";
@@ -18,9 +18,25 @@ const Header = () => {
 					)}
 				>
 					<img src={logo} loading="eager" alt="logo" width="32" height="32" />
-					<Title order={1} fz="h3" mb={4} visibleFrom="sm">
-						Top Pressure
-					</Title>
+					<Group gap="xs">
+						<Title order={1} fz="h3" mb={4} visibleFrom="sm">
+							Top Pressure
+						</Title>
+						{user?.isAuthor && (
+							<>
+								<Divider
+									orientation="vertical"
+									size="sm"
+									h={24}
+									my="auto"
+									visibleFrom="sm"
+								/>
+								<Title order={1} fz="h3" mb={4} visibleFrom="sm">
+									Editor
+								</Title>
+							</>
+						)}
+					</Group>
 				</Group>
 
 				<HeaderRight user={user} logout={logout} />
