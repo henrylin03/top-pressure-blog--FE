d12e7e73f4ccf8adfa5d3196b59683746f25a46f
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-05-27 16:20:05 +1000

    feat(EditPostPage): input fields have values injected from fetching from server (db)
    these aren't editable yet - might use a forms library to manage this... haven't decided yet

--- src/pages/author/EditPostPage.tsx
+++ src/pages/author/EditPostPage.tsx
@@ -4,25 +4,38 @@ import {
 	Group,
 	Input,
 	Stack,
+	Text,
 	Textarea,
 	Title,
 } from "@mantine/core";
+import { useParams } from "react-router";
+import { useFetchPost } from "@/hooks/useFetchPost";
 
 const EditPostPage = () => {
+	const { postId } = useParams();
+	const { post, isLoading } = useFetchPost(String(postId));
+
 	return (
 		<>
 			<title>Edit post | Top Pressure Blog</title>
+			{isLoading && <p>Fetching your post...</p>}
 			<Container px={{ base: "lg", sm: "xl" }} py="xl">
 				<Stack gap="md">
-					<Title order={2} size="h1">
-						Edit post
-					</Title>
+					<Stack gap={0}>
+						<Text fz="xs" c="dimmed" mb={-4}>
+							{post?.isPublished ? "Published" : "Draft"}
+						</Text>
+						<Title order={2} size="h1">
+							Edit post
+						</Title>
+					</Stack>
 					<Stack gap="md">
 						<Input
 							placeholder="Title*"
 							required
 							aria-label="Title of blog post"
 							size="md"
+							value={post?.title}
 						/>
 						<Textarea
 							size="md"
@@ -31,6 +44,7 @@ const EditPostPage = () => {
 							autosize
 							minRows={2}
 							maxRows={3}
+							value={post?.lede}
 						/>
 						<Textarea
 							size="md"
@@ -40,6 +54,7 @@ const EditPostPage = () => {
 							minRows={4}
 							maxRows={6}
 							required
+							value={post?.text}
 						/>
 					</Stack>
 					<Group justify="end" gap="xs" mt="lg">
